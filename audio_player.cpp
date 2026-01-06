d811260cea29dbc49d4c17f06c9c8030722b09d5
Tash <tsukimi.ukiyo@gmail.com>
2026-01-06 06:19:45 +0000

    Introduced structs for decoded data, a function to retrieve the parameters/metadata of audiofiles and started with alsa params

--- audio_player.cpp
+++ audio_player.cpp
@@ -5,15 +5,53 @@
 #include <cstring>
 #include "cstddef"
 #include <vector>
+#include <cstdlib>
+#include <cstdio>
+#include <filesystem>
+#include <sys/syscall.h>
+#include <unistd.h>
 extern "C" {
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
+#include <alsa/asoundlib.h>
 }
+
+
+const char* filename;
+
+// Example usage
+std::unordered_map<std::string, std::string> ffplayFmtMap = {
+    {"s16",  "s16le"},
+    {"s16p", "s16le"},
+    {"s32",  "s32le"},
+    {"s32p", "s32le"},
+    {"fltp", "pcm_f32le"},
+    {"flt",  "f32le"},
+    {"dblp", "f64le"},
+    {"dbl",  "f64le"}
+};
 
 std::unordered_map<std::string, AVCodecID> ext_map = {
     {"mp3", AV_CODEC_ID_MP3},
     {"aac", AV_CODEC_ID_AAC},
     {"wav", AV_CODEC_ID_WMAV1}
+};
+
+
+struct audioInfo {
+    unsigned int sample_rate;
+    int          channels;
+    AVSampleFormat sample_fmt; 
+};
+
+struct DecodedData {
+    AVFrame *frame;
+    AVPacket *packet;
+};
+
+struct AlsaParams {
+    snd_pcm_t *pcm;
+    _snd_pcm_hw_params *params;
 };
 
 struct AudioBytes {
@@ -26,6 +64,98 @@ struct CodecWrapper {
 };
 
 
+static int get_format_from_sample_fmt(const char **fmt,
+                                      enum AVSampleFormat sample_fmt)
+{
+    struct sample_fmt_entry {
+        AVSampleFormat sample_fmt;
+        const char *fmt_be;
+        const char *fmt_le;
+    };
+
+    static const sample_fmt_entry sample_fmt_entries[] = {
+        { AV_SAMPLE_FMT_U8,  "u8",    "u8"    },
+        { AV_SAMPLE_FMT_S16, "s16be", "s16le" },
+        { AV_SAMPLE_FMT_S32, "s32be", "s32le" },
+        { AV_SAMPLE_FMT_FLT, "f32be", "f32le" },
+        { AV_SAMPLE_FMT_DBL, "f64be", "f64le" },
+    };
+
+    *fmt = nullptr;
+
+    for (const auto& entry : sample_fmt_entries) {
+        if (sample_fmt == entry.sample_fmt) {
+            *fmt = AV_NE(entry.fmt_be, entry.fmt_le);
+            return 0;
+        }
+    }
+
+    fprintf(stderr,
+            "sample format %s is not supported as output format\n",
+            av_get_sample_fmt_name(sample_fmt));
+    return -1;
+}
+
+audioInfo getAudioParams() {
+    audioInfo audioMetadata;
+    //std::string str(filename);
+    std::string command = "ffprobe -v error -select_streams a:0 "
+                          "-show_entries stream=sample_rate,channels,sample_fmt "
+                          "-of csv=p=0 \"" + std::string(filename) + "\"";
+
+    FILE* pipe = popen(command.c_str(), "r");
+        if (!pipe) {
+            std::cerr << "Error while reading pipe (seems empty)\n";
+            exit(1);
+        }    
+    char buffer[128];
+    std::string output;
+    while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
+        output += buffer;
+    }
+    if (!output.empty() && output.back() == '\n'){
+        output.pop_back();
+    }
+    std::cout << "RAW OUTPUT: "<< output << std::endl;
+    std::stringstream STREAM(output);
+
+   
+    std::string sample_rate_str;
+    std::string channels_str;
+    std::string sample_fmt_str;
+
+    std::getline(STREAM, sample_rate_str, ',');
+    std::getline(STREAM, channels_str, ',');
+    std::getline(STREAM, sample_fmt_str, ',');
+
+    audioMetadata.sample_rate = std::stoul(sample_rate_str);
+    audioMetadata.channels    = std::stoi(channels_str);
+    audioMetadata.sample_fmt  = av_get_sample_fmt(sample_fmt_str.c_str());
+
+    /**
+     * getline() is kind of like .split() in python
+     * */
+    if (audioMetadata.sample_fmt == AV_SAMPLE_FMT_NONE) {
+        std::cerr << "Unknown sample format: " << sample_fmt_str << "\n";
+        exit(1);
+    }
+    // ---- PRINT RESULTS ----
+    std::cout << "Parsed metadata:\n";
+    std::cout << "  Sample Format: " << audioMetadata.sample_fmt << "\n";
+    std::cout << "  Sample Rate:   " << audioMetadata.sample_rate << "\n";
+    std::cout << "  Channels:      " << audioMetadata.channels << "\n";
+    
+    std::cout << "END\n"; 
+    return audioMetadata;
+
+}
+
+void send_to_sd(){
+    std::cout << "Starting audio param extraction process"; 
+  audioInfo Params = getAudioParams();
+
+}
+
 // returns the file extension
 const char* getExt(const char* f) {
     
@@ -35,6 +165,7 @@ const char* getExt(const char* f) {
 
 
 void decode(CodecWrapper w, AVPacket *packet, AVFrame **frame) {
+    std::cout << "Attempting decoding process";
     int ret, data_size;
     AudioBytes byteinfo;
 
@@ -46,6 +177,7 @@ void decode(CodecWrapper w, AVPacket *packet, AVFrame **frame) {
 
     AVFrame* f = *frame;
     while (true) {
+        // getting frames
         ret = avcodec_receive_frame(w.ctx, f);
         if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF) {
             return;
@@ -53,39 +185,39 @@ void decode(CodecWrapper w, AVPacket *packet, AVFrame **frame) {
             std::cerr << "Error during decoding\n";
             exit(1);
         }
-
+        // getting samples
         data_size = av_get_bytes_per_sample(w.ctx->sample_fmt);
         if (data_size < 0) {
             std::cerr << "Failed to calculate data size\n";
             exit(1);
         }
-
+        // Writing to data structure
         for (int i = 0; i < f->nb_samples; i++) {
             for (int ch = 0; ch < w.ctx->ch_layout.nb_channels; ch++) {
                 std::byte* sample_ptr = reinterpret_cast<std::byte*>(f->data[ch] + data_size*i);
                 for (int b = 0; b < data_size; ++b) {
                     byteinfo.data.push_back(sample_ptr[b]);
+                    }
                 }
             }
         }
-    }
+        send_to_sd();
+
+
 }
-void open_codec(CodecWrapper wrapper){
+DecodedData open_codec(CodecWrapper wrapper){
+    std::cout << "Beginning decoding process";
+    DecodedData data;
     if (avcodec_open2(wrapper.ctx, wrapper.codec, NULL)<0){
         exit(0);
     }
-    AVFrame *frame  = av_frame_alloc();
-    AVPacket *pkt   = av_packet_alloc();
+    data.frame    = av_frame_alloc();
+    data.packet   = av_packet_alloc();
     std::cout << "Preparing memory allocation\n";
-    if (!frame ||  !pkt){
+    if (!data.frame ||  !data.packet){
         std::cerr << "Could not allocate space for reading" << std::endl;
-    }else{
-        void decode();
     }
-    
-
-   av_frame_free(&frame);
-   av_packet_free(&pkt); 
+    return data;
 }
 
 CodecWrapper createCodec(const char* path) {
@@ -121,6 +253,8 @@ CodecWrapper createCodec(const char* path) {
 }
 
 void playAudio(const char* filepath) {
+    DecodedData data;
+    //AVFrame
     // Gets audio codec and alloctes it to the CodecWrapper struct
     CodecWrapper wrapper = createCodec(filepath);
     if (!wrapper.codec || !wrapper.ctx) {
@@ -130,8 +264,12 @@ void playAudio(const char* filepath) {
 
     std::cout << "Decoder ready for " << filepath << "\n";
 
-    open_codec(wrapper);
+    data = open_codec(wrapper);
+        
+    void decode();
 
+    av_frame_free(&data.frame);
+    av_packet_free(&data.packet); 
     avcodec_free_context(&wrapper.ctx);
 }
 
@@ -140,7 +278,7 @@ int main(int argc, char** argv) {
         std::cerr << "Usage: player <audiofile>\n";
         return 1;
     }
-
+    filename = argv[1];
     playAudio(argv[1]);
     return 0;
 }
