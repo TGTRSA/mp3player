c60019fb08c58fa1a20394bae8b607acbeff7af4
TGTRSA <tsukimi.ukiyo@gmail.com>
2026-01-27 17:25:03 +0000

    [UNTESTED] Wrote functions to open and configure alsa dev and structs for both the alsa handle and audio metadata

--- audio_player.cpp
+++ audio_player.cpp
@@ -31,20 +31,48 @@ struct AlsaHandle {
     int                 mode        = 0 ;
 };
 
-void open_alsa_dev(){
-    AlsaHandle alsa;
+struct AudioMetadata {
+    int channels;
+    int sample_rate;
+};
+
+int configure_params(AlsaHandle alsa, AudioMetadata audiodata){
+    int rc = snd_pcm_set_params(
+        alsa.pcm,
+        SND_PCM_FORMAT_FLOAT_LE,
+        SND_PCM_ACCESS_RW_INTERLEAVED,
+        audiodata.channels,
+        audiodata.sample_rate,
+        1,        // soft_resample
+        50000     // latency in microseconds
+    );
+    return rc;
+}
+
+int open_alsa_dev(AlsaHandle alsa){
     // 1. Open PCM device
     int rc = snd_pcm_open(&alsa.pcm, alsa.device_name, alsa.stream_type, alsa.mode);
-    if (rc < 0) {
-        fprintf(stderr, "Could not open ALSA device: %s\n", snd_strerror(rc));
-    }
-
+    return rc;
 }
 
 
 // int argc, char[] * argv 
 int main() {
+    AlsaHandle alsa;
+    AudioMetadata metadata;
+    int rc;
     std::cout << "Starting program "<<std::endl ;
+    rc = open_alsa_dev(alsa);
+    if (rc < 0) {
+        fprintf(stderr, "Could not open ALSA device: %s\n", snd_strerror(rc));
+    }
+
+    rc = configure_params(alsa,metadata);
+    if (rc < 0) {
+        fprintf(stderr, "Unable to set PCM parameters: %s\n", snd_strerror(rc));
+        return 1;
+    }
+
 
 
 
