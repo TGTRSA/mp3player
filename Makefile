62270bf09996547aa5e2199828759d35265758cc
TGTRSA <tsukimi.ukiyo@gmail.com>
2026-02-03 20:57:46 +0000

    [BUG FIX] Can now sfely open and configure alsa params the cause of the bug was incorrect association of metadata which caused the set params block to segfault

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 
 CC = g++
-SOURCES = audio_player.cpp
+SOURCES = audio_player.cpp 
 #SOURCES =  pcm.cpp
 OUTPUT = audio_player
 #OUTPUT = pcm
-CFLAGS = -I/usr/include -L/usr/lib -lavformat -lavcodec -lavutil -lasound 
+CFLAGS = -g -O0 -Wall -Wextra -I/usr/include -L/usr/lib -lavformat -lavcodec -lavutil -lasound 
 
 
 all:
--- audio_player.cpp
+++ audio_player.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <alsa/asoundlib.h>
 #include <sched.h>
+#include <useful_funcs.h>
 //#include <typeinfo>
 //#include <errno.h>
 #include <getopt.h>
@@ -26,14 +27,17 @@ extern "C" {
 
 // Represents the entire media file => All of its "byte" data
 struct Stream {
-    AVFormatContext     *av_fmt_ctx     = NULL;
+
+    AVFormatContext     *av_fmt_ctx     = nullptr;
+
     AVMediaType         media_type      = AVMEDIA_TYPE_AUDIO;
-    int                 audio_stream;
+
+    int                 audio_stream    = -1 ;
 };
 
 struct AlsaHandle {
     snd_pcm_t           *pcm;
-    const char *        device_name = "default";
+    const char*         device_name = "default";
     snd_pcm_uframes_t   buffer_size = 1024; // size of frames: usgined int => either 1 or 2 stereo or mono
     snd_pcm_stream_t    stream_type = SND_PCM_STREAM_PLAYBACK;
     int                 mode        = 0 ;
@@ -44,36 +48,37 @@ struct AudioMetadata {
     int sample_rate;
 };
 
-Stream get_audio_streams(const char * filename) {
+Stream get_audio_streams(const char *filename) {
+    Stream s;// Initialize with safe defaults
     int rc;
-    Stream s;
-    AVDictionary *dict_opt = NULL;
 
-    s.av_fmt_ctx = avformat_alloc_context();
-    std::cout << "allocating context to avformat\n";
-    if (!s.av_fmt_ctx) {
-        fprintf(stderr, "Could not allocate AVFormatContext\n");
+    // Open input: handles allocation if s.av_fmt_ctx is NULL
+    rc = avformat_open_input(&s.av_fmt_ctx, filename, NULL, NULL);
+    if (rc < 0) {
+        char errbuf[256];
+        av_strerror(rc, errbuf, sizeof(errbuf));
+        std::cerr << "Could not open " << filename << ": " << errbuf << std::endl;
+        return s; 
+    }
+
+    // Retrieve stream information
+    if (avformat_find_stream_info(s.av_fmt_ctx, NULL) < 0) {
+        std::cerr << "Could not find stream information" << std::endl;
+        avformat_close_input(&s.av_fmt_ctx);
         return s;
     }
 
-    rc = avformat_open_input(&s.av_fmt_ctx,filename ,NULL , NULL);
-    std::cout << "Opening avformat\n";
-    if (rc<0){
-        fprintf(stderr, "Could not open file stream\n");
-        avformat_free_context(s.av_fmt_ctx);
+    // Find the best audio stream
+    s.audio_stream = av_find_best_stream(s.av_fmt_ctx, AVMEDIA_TYPE_AUDIO, -1, -1, NULL, 0);
+    if (s.audio_stream < 0) {
+        std::cerr << "Could not find an audio stream in " << filename << std::endl;
     }
-
-    avformat_find_stream_info(s.av_fmt_ctx, &dict_opt);
-    std::cout << "Collecting streams\n";
-    s.audio_stream = av_find_best_stream(
-        s.av_fmt_ctx, AVMEDIA_TYPE_AUDIO, -1, -1, NULL, 0
-    );
 
     return s;
 }
 
 void play_audio(AlsaHandle alsa, AudioMetadata audiometada, const void * pcm_data){
-    float buffer;
+    //float buffer;
     int rc;
     int offset = 0;
     while(offset< audiometada.sample_rate){ //doing loop for number of frames => sample rate
@@ -109,9 +114,14 @@ int configure_params(AlsaHandle alsa, AudioMetadata audiodata){
     return rc;
 }
 
-int open_alsa_dev(AlsaHandle alsa){
+int open_alsa_dev(AlsaHandle &alsa){
     // 1. Open PCM device
+
     int rc = snd_pcm_open(&alsa.pcm, alsa.device_name, alsa.stream_type, alsa.mode);
+    if(rc<0){
+        std::cout << "Error opening alsa";
+        print_nl();
+    }
     return rc;
 }
 
@@ -120,7 +130,7 @@ int open_alsa_dev(AlsaHandle alsa){
 int main() {
     AlsaHandle alsa;
     AudioMetadata metadata;
-    Stream s;
+    
     int rc;
     std::cout << "Starting program "<<std::endl ;
 
@@ -134,17 +144,26 @@ int main() {
         std::cout << path << " exists" <<std::endl;
     }
 
-    s = get_audio_streams(path);
-    if (!(s.audio_stream>0)){
+    Stream s = get_audio_streams(path);
+    if (s.audio_stream < 0){
         fprintf(stderr, "Error no audio streams");
         return 0;
     }
+
+    AVCodecParameters *params = s.av_fmt_ctx->streams[s.audio_stream]->codecpar;
+    metadata.channels = params->ch_layout.nb_channels; 
+    metadata.sample_rate = params->sample_rate;
+
+
     rc = open_alsa_dev(alsa);
+    std::cout << "Opening alsa dev" << std::endl;
     if (rc < 0) {
         fprintf(stderr, "Could not open ALSA device: %s\n", snd_strerror(rc));
     }
-
+    std::cout << "Finished opening ALSA dev";
+    print_nl();
     rc = configure_params(alsa,metadata);
+    std::cout <<  "Configuring params for alsa" << std::endl;
     if (rc < 0) {
         fprintf(stderr, "Unable to set PCM parameters: %s\n", snd_strerror(rc));
         return 1;
